b0cdd35b57bab70160ce0e1ce5625db4a420fbd2
Murtaza Pakawala <mypaka52@gmail.com>
2022-08-25 14:51:36 +1000

    logic for game: filling alphabets inside the wordle mao

--- App.js
+++ App.js
@@ -1,31 +1,81 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
-import { colors } from "./src/constants";
+import { colors, ENTER, CLEAR } from "./src/constants";
 import Keyboard from "./src/components/Keyboard";
+import { useState } from "react";
 
 const NUMBER_OF_TRIES = 5;
+function copyArray(arr) {
+  return [...arr.map((row) => [...row])];
+}
 export default function App() {
-  const word = "someone";
+  const word = "hello";
   const letters = word.split("");
-  const rows = new Array(NUMBER_OF_TRIES).fill(
-    new Array(letters.length).fill("a")
+  //state for whole map
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(""))
   );
+  //for adding the value at the correct position
+  const [currRow, setCurrRow] = useState(0);
+  const [currCol, setCurrCol] = useState(0);
+  //when key pressed adding the alphabet at correct postion
+  const onKeyPressed = (key) => {
+    //copy array since cant change the original state
+    const newCopy = copyArray(rows);
+
+    //if clear
+    if (key === CLEAR) {
+      if (currCol > 0) {
+        newCopy[currRow][currCol - 1] = "";
+        setCurrCol(currCol - 1);
+        setRows(newCopy);
+      }
+      return;
+    }
+    //if enter going to next row
+    if (key === ENTER) {
+      if (currRow < rows.length && currCol === rows[0].length) {
+        setCurrRow(currRow + 1);
+        setCurrCol(0);
+      }
+      return;
+    }
+    //moving to next col cell
+    if (currCol < rows[0].length) {
+      newCopy[currRow][currCol] = key;
+      setCurrCol(currCol + 1);
+      setRows(newCopy);
+    }
+  };
+  const isCellActive = (i, j) => {
+    return i === currRow && j === currCol;
+  };
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <Text style={styles.title}>WORDLE</Text>
       <ScrollView style={styles.map}>
-        {rows.map((row) => (
-          <View style={styles.row}>
-            {letters.map((cell) => (
-              <View style={styles.cell}>
-                <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+        {rows.map((row, i) => (
+          <View key={`row-${i}`} style={styles.row}>
+            {row.map((cell, j) => (
+              <View
+                key={`col-${j}`}
+                style={[
+                  styles.cell,
+                  {
+                    borderColor: isCellActive(i, j)
+                      ? colors.lightgrey
+                      : colors.darkgrey,
+                  },
+                ]}
+              >
+                <Text style={[styles.cellText]}>{cell.toUpperCase()}</Text>
               </View>
             ))}
           </View>
         ))}
       </ScrollView>
-      <Keyboard />
+      <Keyboard onKeyPressed={onKeyPressed} />
     </SafeAreaView>
   );
 }
